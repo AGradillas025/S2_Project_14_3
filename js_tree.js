6c3872d4a394e67cf1da410d957592328b8a06f7
Unknown <AGradillas025@west-mec.org>
2019-04-09 18:18:02 -0700

    Final Commit
    This is my final commit and is ready for review. In this final commit I finished the makeTree function and fixed a few mistakes and added in comments while adding in a few if statements.

--- js_tree.js
+++ js_tree.js
@@ -38,10 +38,10 @@
 */
 
 // Keeps track of the total count of nodes, element nodes, text nodes, and white-space nodes
-nodeCount = 0;
-elemCount = 0;
-textCount = 0;
-wsCount = 0;
+var nodeCount = 0;
+var elemCount = 0;
+var textCount = 0;
+var wsCount = 0;
 
 // Runs the makeTree function when the page loads
 window.onload = makeTree;
@@ -60,11 +60,54 @@ function makeTree() {
 
       var sourceArticle = document.querySelector("#main article");
       makeBranches(sourceArticle, nodeList);
+
+      document.getElementById("totalNodes").textContent = nodeCount;
+      document.getElementById("elemNodes").textContent = elemCount;
+      document.getElementById("textNodes").textContent = textCount;
+      document.getElementById("wsNodes").textContent = wsCount;
 }
 
 // The function makeBranches will append node branches to the node tree
 function makeBranches(treeNode, nestedList) {
       nodeCount++;
+      var liElem = document.createElement("li");
+      liElem.innerHTML = "+--";
+      var spanElem = document.createElement("span");
+      liElem.appendChild(spanElem);
+      nestedList.appendChild(liElem);
+
+      if (treeNode.nodeType === 1) {
+            // If the treeNode represents an element it will increase the elemCount variable by 1
+            elemCount++;
+            spanElem.setAttribute("class", "elementNode");
+            // The textContent will append the text of the element tag
+            document.textContent = "<" + treeNode.nodeName + ">";
+      } else if (treeNode.nodeType === 3) {
+            // If else the treeNode represents an element it will increase the textCount variable by 1
+            textCount++;
+            var textString = treeNode.nodeValue;
+
+            if (isWhiteSpaceNode(textString)) {
+                  // If the isWhiteSpaceNode with a parameter of textstring is true then the wsCount variable will increase by 1
+                  wsCount++;
+                  spanElem.setAttribute("class", "WhiteSpaceNode");
+                  spanElem.textContent = "#text";
+            } else {
+                  spanElem.setAttribute("class", "textNode");
+                  spanElem.textContent = textString;
+            }
+      }
+
+      // Determining if the treeNode is false should help the makeBranches function move recursively through the different levels of node throught the article
+      if (treeNode.childNodes.length > 0) {
+            var newList = document.createElement("ol");
+            newList.innerHTML = "|";
+
+            nestedList.appendChild(newList);
+            for (var n = treeNode.firstChild; n !== null; n = n.nextSibling) {
+                  makeBranches(n, newList);
+            }
+      }
 }
 
 function isWhiteSpaceNode(tString) {
